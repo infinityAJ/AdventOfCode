cdaca1ac4fe66a9df093a3890257156192cf0a35
InfinityAJ <jainanant597@gmail.com>
2022-12-17 13:41:50 +0530

    day 10 problem 2 done
    Signed-off-by: InfinityAJ <jainanant597@gmail.com>

---
+++ src/day10/Problem2.java
@@ -0,0 +1,35 @@
+package day10;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+public class Problem1 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(new File(args[0]));
+        int cycle = 0;
+        int x = 1;
+        while(sc.hasNextLine()) {
+            String s = sc.nextLine();
+            if(s.charAt(0)=='n') {
+                print(x, cycle);
+                cycle++;
+            } else {
+                print(x, cycle);
+                cycle++;
+                print(x, cycle);
+                cycle++;
+                x += Integer.parseInt(s.split(" ")[1]);
+            }
+        }
+    }
+
+    private static void print(int x, int cycle) {
+        int c = cycle%40;
+        if(c==0) System.out.print('\n');
+        if(c>=x-1 && c<=x+1) {
+            System.out.print('#');
+        } else {
+            System.out.print('.');
+        }
+    }
+}
